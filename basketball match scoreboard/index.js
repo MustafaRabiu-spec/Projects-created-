3645d698a8088c7cdd63da6f8e69fd375e80163f
MustafaRabiu-spec <mustafalanrerabiu@gmail.com>
2025-12-14 18:06:10 +0100

    Add files via upload

---
+++ basketball match scoreboard/index.js
@@ -0,0 +1,38 @@
+let num1 = 0
+let num2 = 0
+
+let sum1EL = document.getElementById("num1-el")
+let sum2EL = document.getElementById("num2-el")
+
+function add1() {
+    num1 = num1 + 1
+    sum1EL.textContent = num1
+}
+
+
+function add2() {
+    num1 = num1 + 2
+    sum1EL.textContent = num1
+}
+
+function add3() {
+    num1= num1 + 3
+    sum1EL.textContent = num1
+}
+
+function add11() {
+    num2 = num2 + 1
+    sum2EL.textContent = num2
+}
+
+function add22() {
+    num2 = num2 + 2
+    sum2EL.textContent = num2
+}
+
+function add33() {
+    num2 = num2 + 3
+    sum2EL.textContent = num2
+}
+
+
